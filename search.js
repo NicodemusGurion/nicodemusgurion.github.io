c9a5a15135be7aa06232a299126242de6d1f3b15
NicodemusGurion <93310458+NicodemusGurion@users.noreply.github.com>
2023-04-09 09:01:35 +0200

    Create search.js

---
+++ search.js
@@ -0,0 +1,9 @@
+var SearchDatabase = {}
+
+// this function is called when the user taps the search bar to lazy-load the search database
+function loadDatabase()
+{
+  if SearchDatabase == {}
+  {
+  }
+}
