e306459c775de7b008aaa38c906e8a205e442156
NicodemusGurion <93310458+NicodemusGurion@users.noreply.github.com>
2023-04-24 00:37:44 +0200

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -13,3 +13,75 @@
     checkbox.checked = false;
   }, false);
 })(document);
+
+
+var SearchDatabase;
+
+	function loadDatabase()
+	{
+		if (SearchDatabase === undefined)
+		{
+		  fetch("/database.json")
+		  .then(response => response.json())
+		  .then(data => {
+			SearchDatabase = data;
+		  })	
+		}
+	}
+  
+  function searchInDatabase(query)
+  {
+    if (SearchDatabase === undefined)
+    {
+	  document.querySelector("#searchResults").innerHTML = "Database not loaded.";
+	  return;
+    }
+    var queryWords = query.toLowerCase().trim().split(" ");
+    if (query.trim().length == "")
+    {
+    	document.querySelector("#searchResults").innerHTML = "No results.";
+    	return;
+    }
+    var foundMatches = {};
+    for (queryWord of queryWords)
+    {
+    	var queryKey = queryWord
+    	if (!(queryWord in SearchDatabase.searchdata)) //if the key does not exist, look for a partial match
+    	{
+    	  for (const [key, value] of Object.entries(SearchDatabase.searchdata)) {
+			if (key.startsWith(queryWord)) {
+			console.log(key);
+			  queryKey = key;
+			  break;
+			}
+		  }
+    	}
+		if (queryKey in SearchDatabase.searchdata)
+    	{    	
+    		for (const key in SearchDatabase.searchdata[queryKey])
+    	  	{
+    	  		if (key in foundMatches)
+    	  		{ 
+    	  			foundMatches[key] += SearchDatabase.searchdata[queryKey][key];
+    	  		}
+    	  		else
+    	  		{
+    	  			foundMatches[key] = SearchDatabase.searchdata[queryKey][key];
+    	  		}
+    	  	}
+    	}
+    }
+	if (Object.keys(foundMatches).length === 0)
+    {
+    	document.querySelector("#searchResults").innerHTML = "No results.";
+    	return;
+    }
+    const fm_arr = Object.entries(foundMatches);
+    fm_arr.sort((a, b) => b[1] - a[1]);
+    output = "<ul>";
+    for (const [key, value] of fm_arr) {
+		output += "<li><a href=\"" + SearchDatabase.postlist[key].url + "\">" + SearchDatabase.postlist[key].title + "</a></li>";  	
+    }
+    output += "</ul>";
+    document.querySelector("#searchResults").innerHTML = output;
+  }
