c9d7d25d5efe92a8abfcf5d5886f260b25645652
NicodemusGurion <93310458+NicodemusGurion@users.noreply.github.com>
2024-03-24 10:52:33 +0100

    Update QuranRefs.js

--- public/js/QuranRefs.js
+++ public/js/QuranRefs.js
@@ -1,20 +1,17 @@
 function QuranRefs() {
     var mainDiv = document.getElementById("thecontent");
     var text = mainDiv.innerHTML;
-  console.log(text);
     const regex = /(surah[s]*\s*[0-9\:\,\-\;\s]*[0-9])/gi;
     mainDiv.innerHTML = text.replace(regex, (match, surah) => SurahLink(surah));
 }
 
 function SurahLink(input) {
-  console.log("surah", input);
   const surahs = input.split(';');
   const chapterLinks = surahs.map(chapter => ChapterLink(chapter));
   return chapterLinks.join(';');
 }
 
 function ChapterLink(input) {
-  console.log("chapter", input);
   const [chapterStr, verses] = input.split(':', 2);
   const chapter = chapterStr.replace(/[^\d]/g, '');
   const versesLinks = input.split(",").map(verses => VersesLink(chapter, verses));
@@ -22,12 +19,6 @@ function ChapterLink(input) {
 }
 
 function VersesLink(chapter, verses) {
-  console.log("verse",chapter, verses);
   const versesStripped = verses.replace(/^[^:]*:/g, '').replace(/[^\d-]/g, '');
-  return `<a href="https://quran.com/${chapter}/${versesStripped}" target="_BLANK">${verses}</a>`;
+  return `<a href="https://quran.com/${chapter}/${versesStripped}" target="_blank">${verses}</a>`;
 }
-
-/*function parseAndReplace(text) {
-  const regex = /(surah[s]*\s*[0-9\:\,\-\;\s]*[0-9])/gi;
-  return text.replace(regex, (match, surah) => SurahLink(surah.trim()));
-}*/
