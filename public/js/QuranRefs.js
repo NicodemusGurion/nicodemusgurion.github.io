5da4c3e5650c13817baef32742cd8410004f041b
NicodemusGurion <93310458+NicodemusGurion@users.noreply.github.com>
2024-03-24 10:00:20 +0100

    Update QuranRefs.js

--- public/js/QuranRefs.js
+++ public/js/QuranRefs.js
@@ -1,30 +1,29 @@
 function QuranRefs() {
     var mainDiv = document.getElementById("thecontent");
     var text = mainDiv.innerText;
-    const regex = /surah[s]*\s*[0-9\:\,\-\;\s]*/gi;
-    mainDiv.innerHTML = text.replace(regex, (match, surah) => SurahLink(surah.trim()));
-    });
-};
-
+    const regex = /(surah[s]*\s*[0-9\:\,\-\;\s]*[0-9])/gi;
+    mainDiv.innerHTML = text.replace(regex, (match, surah) => SurahLink(surah));
+}
 
 function SurahLink(input) {
-    const surahs = input.split(';');
-    const chapterLinks = surahs.map(chapter => ChapterLink(chapter));
-    return chapterLinks.join(';');
+  const surahs = input.split(';');
+  const chapterLinks = surahs.map(chapter => ChapterLink(chapter));
+  return chapterLinks.join(';');
 }
 
 function ChapterLink(input) {
-    const chapter = parseInt(input.match(/\d+/)[0], 10);
-    const versesLinks = input.split(',').map(verse => VersesLink(chapter, verse.trim()));
-    return versesLinks.join(', ');
+  const [chapterStr, verses] = input.split(':', 2);
+  const chapter = chapterStr.replace(/[^\d]/g, '');
+  const versesLinks = input.split(",").map(verses => VersesLink(chapter, verses));
+  return versesLinks.join(', ');
 }
 
 function VersesLink(chapter, verses) {
-    const versesStripped = verses.split(':')[1].replace(/[^\d-]/g, '');
-    return `<a href="https://quran.com/${chapter}/${versesStripped}">${verses}</a>`;
+  const versesStripped = verses.replace(/^[^:]*:/g, '').replace(/[^\d-]/g, '');
+  return '<a href="https://quran.com/${chapter}/${versesStripped}">${verses}</a>';
 }
 
-function parseAndReplace(text) {
-    const regex = /(surah[s]*\s*)([0-9\:\,\-\;\s]*)/gi;
-    return text.replace(regex, (match, surah) => SurahLink(surah.trim()));
-}
+/*function parseAndReplace(text) {
+  const regex = /(surah[s]*\s*[0-9\:\,\-\;\s]*[0-9])/gi;
+  return text.replace(regex, (match, surah) => SurahLink(surah.trim()));
+}*/
