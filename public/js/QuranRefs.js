4b6da021b33c3808d083a8ba04689b3b2a54dd53
NicodemusGurion <93310458+NicodemusGurion@users.noreply.github.com>
2024-03-24 10:05:36 +0100

    Update QuranRefs.js

--- public/js/QuranRefs.js
+++ public/js/QuranRefs.js
@@ -1,17 +1,20 @@
 function QuranRefs() {
     var mainDiv = document.getElementById("thecontent");
     var text = mainDiv.innerText;
+  console.log(text);
     const regex = /(surah[s]*\s*[0-9\:\,\-\;\s]*[0-9])/gi;
     mainDiv.innerHTML = text.replace(regex, (match, surah) => SurahLink(surah));
 }
 
 function SurahLink(input) {
+  console.log("surah", input);
   const surahs = input.split(';');
   const chapterLinks = surahs.map(chapter => ChapterLink(chapter));
   return chapterLinks.join(';');
 }
 
 function ChapterLink(input) {
+  console.log("chapter", input);
   const [chapterStr, verses] = input.split(':', 2);
   const chapter = chapterStr.replace(/[^\d]/g, '');
   const versesLinks = input.split(",").map(verses => VersesLink(chapter, verses));
@@ -19,6 +22,7 @@ function ChapterLink(input) {
 }
 
 function VersesLink(chapter, verses) {
+  console.log("verse",chapter, verses);
   const versesStripped = verses.replace(/^[^:]*:/g, '').replace(/[^\d-]/g, '');
   return '<a href="https://quran.com/${chapter}/${versesStripped}">${verses}</a>';
 }
