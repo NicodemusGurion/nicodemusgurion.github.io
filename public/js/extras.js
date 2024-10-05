00a3fedd17c7eaa507c48e14161105a2ca366879
Gazuag <gazuag@mail.com>
2024-10-05 13:51:40 +0200

    Update default.html, quran_link_filter.rb and two other files

--- public/js/extras.js
+++ public/js/extras.js
@@ -37,7 +37,7 @@ const hadithMappings = {
 // Function to convert Hadith references to links
 function convertHadithReferences() {
   // Get the content of the page (or a specific container)
-  const bodyContent = document.body.innerHTML;
+  const bodyContent = document.getElementById('maincontent');
 
   // Loop through each mapping and apply the replacement
   for (const [hadithName, urlKeyword] of Object.entries(hadithMappings)) {
@@ -51,6 +51,44 @@ function convertHadithReferences() {
   }
 }
 
-// Run the conversion function after the DOM content is loaded
-document.addEventListener("DOMContentLoaded", QuranRefs);
-document.addEventListener("DOMContentLoaded", convertHadithReferences);
+
+function generateTableOfContents() {
+  var mainContent = document.getElementById('maincontent');
+  if (!mainContent) {
+    return;
+  }
+
+  var headers = mainContent.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  var toc = '<h1>Table of contents</h1>';
+  var currentLevel = 0;
+
+  for (var i = 0; i < headers.length; i++) {
+    var header = headers[i];
+    var level = parseInt(header.tagName.charAt(1));
+
+    if (level > currentLevel) {
+      toc += '<ul>';
+    } else if (level < currentLevel) {
+      toc += '</li></ul>'.repeat(currentLevel - level) + '</li>';
+    } else {
+      toc += '</li>';
+    }
+
+    toc += '<li><a href="#' + header.id + '">' + header.textContent + '</a>';
+    currentLevel = level;
+  }
+
+  toc += '</li></ul>'.repeat(currentLevel);
+
+  var mainContentText = mainContent.innerHTML;
+  mainContentText = mainContentText.replace('[TOC]', toc);
+  mainContent.innerHTML = mainContentText;
+}
+
+function runAllExtras(){
+generateTableOfContents();
+QuranRefs();
+convertHadithReferences()
+}
+
+document.addEventListener("DOMContentLoaded", runAllExtras);
