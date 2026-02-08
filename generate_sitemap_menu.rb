320d712aa1b23d9d31652322b41ed1dc2feaf224
Gazuag <gazuag@mail.com>
2026-02-08 20:09:12 +0100

    Update child_pages_toc.html and generate_sitemap_menu.rb

--- generate_sitemap_menu.rb
+++ generate_sitemap_menu.rb
@@ -1,97 +1,57 @@
 #!/usr/bin/env ruby
 require 'json'
 
-# Recursively build the sitemap HTML
-def build_menu_html(node, indent_level = 0)
-  return '' if node.nil?
-  return '' unless node['include_in_menu']
+# Read the site structure
+data = JSON.parse(File.read('_data/site_structure.json'))
+
+# Generate HTML for a node and its children recursively
+def generate_html(node, level = 0)
+  html = ""
   
-  html = ''
-  indent = '  ' * indent_level
-  
-  children = node['children'] || {}
-  headers = node['headers'] || []
-  
-  # Filter children to only those with include_in_menu
-  menu_children = children.select { |key, child| child['include_in_menu'] }
-  
-  has_children = !menu_children.empty?
-  has_headers = !headers.empty?
-  
-  # If this node has children or headers, wrap in details
-  if has_children || has_headers
-    html << "#{indent}<li>\n"
-    html << "#{indent}  <details>\n"
-    html << "#{indent}    <summary><a href=\"#{node['url']}\">#{node['title']}</a></summary>\n"
-    html << "#{indent}    <ul>\n"
+  # Generate link for this node (skip root)
+  if node['url'] != '/'
+    indent = "  " * level
+    html += "#{indent}<li>\n"
+    html += "#{indent}  <a href=\"#{node['url']}\">#{node['title']}</a>\n"
     
-    # Add headers first
-    headers.each do |header|
-      html << "#{indent}      <li><a href=\"#{node['url']}##{header['anchor']}\">#{header['text']}</a></li>\n"
+    # Add headers if present
+    if node['headers'] && node['headers'].length > 0
+      html += "#{indent}  <ul class=\"headers\">\n"
+      node['headers'].each do |header|
+        html += "#{indent}    <li><a href=\"#{node['url']}##{header['anchor']}\"><strong>#{header['text']}</strong></a></li>\n"
+      end
+      html += "#{indent}  </ul>\n"
     end
     
-    # Then add child pages
-    menu_children.keys.sort.each do |child_key|
-      child = menu_children[child_key]
-      html << build_menu_html(child, indent_level + 3)
+    # Add children if present
+    if node['children'] && node['children'].length > 0
+      html += "#{indent}  <ul>\n"
+      node['children'].sort.each do |key, child|
+        html += generate_html(child, level + 2)
+      end
+      html += "#{indent}  </ul>\n"
     end
     
-    html << "#{indent}    </ul>\n"
-    html << "#{indent}  </details>\n"
-    html << "#{indent}</li>\n"
+    html += "#{indent}</li>\n"
   else
-    # Just a simple link with no children or headers
-    html << "#{indent}<li><a href=\"#{node['url']}\">#{node['title']}</a></li>\n"
+    # For root, just process children
+    if node['children'] && node['children'].length > 0
+      node['children'].sort.each do |key, child|
+        html += generate_html(child, level)
+      end
+    end
   end
   
   html
 end
 
-# Main processing
-def generate_sitemap_menu
-  # Read the site structure
-  structure_file = '_data/site_structure.json'
-  
-  unless File.exist?(structure_file)
-    puts "Error: #{structure_file} not found. Run generate_structure.rb first."
-    exit 1
-  end
-  
-  data = JSON.parse(File.read(structure_file))
-  hierarchy = data['hierarchy']
-  
-  # Start building HTML
-  html = <<~HTML
-    <nav class="sitemap-menu">
-      <ul>
-  HTML
-  
-  # Add root page if it exists and has meaningful content
-  if hierarchy['url'] && hierarchy['url'] != '/' && hierarchy['include_in_menu']
-    html << "    <li><a href=\"#{hierarchy['url']}\">#{hierarchy['title']}</a></li>\n"
-  end
-  
-  # Build menu from children
-  children = hierarchy['children'] || {}
-  children.keys.sort.each do |child_key|
-    child = children[child_key]
-    html << build_menu_html(child, 2)
-  end
-  
-  html << <<~HTML
-      </ul>
-    </nav>
-  HTML
-  
-  # Ensure _includes directory exists
-  Dir.mkdir('_includes') unless Dir.exist?('_includes')
-  
-  # Write to file
-  output_file = '_includes/sitemap_menu.html'
-  File.write(output_file, html)
-  
-  puts "Generated #{output_file}"
-end
+# Generate the complete menu
+menu_html = "<nav class=\"sitemap-menu\">\n"
+menu_html += "  <ul>\n"
+menu_html += generate_html(data['hierarchy'], 1)
+menu_html += "  </ul>\n"
+menu_html += "</nav>\n"
 
-# Run it
-generate_sitemap_menu
+# Write to file
+File.write('_includes/sitemap_menu.html', menu_html)
+puts "Generated sitemap menu with #{data['flat'].length} pages"
